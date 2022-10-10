96e35014ba8eb583709407f90e5fccc7f12b9d42
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-10-09 21:12:25 -0700

    Add bootstrap, form styling, error styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def new
     @article = Article.new
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -15,11 +16,9 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
-    #render plain: params[:article].inspect
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was updated"
       redirect_to article_path(@article)
@@ -30,14 +29,12 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was destroyed"
     redirect_to articles_path
   end
-  
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -47,5 +44,9 @@ class ArticlesController < ApplicationController
   private
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
     end
 end
